41910fa5f1bc7652ee223b607d9ba9b34eef6cc6
Alan <alan_hahn1995@outlook.com>
2025-01-27 16:14:03 -0300

    Criando as Rotas

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,23 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/ping', function () {
-    return ['pong' => true];
-});
+use App\Http\Controllers\{
+    AuthController,
+    CidadeController,
+    MedicoController,
+    PacienteController
+};
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/user', [AuthController::class, 'user']);
+
+Route::get('/cidades', [CidadeController::class, 'index']);
+Route::get('/cidades/{id_cidade}/medicos', [CidadeController::class, 'listarMedicosCidade']);
+
+Route::get('/medicos', [MedicoController::class, 'index']);
+Route::post('/medicos', [MedicoController::class, 'store']);
+Route::post('/medicos/consulta', [MedicoController::class, 'agendarConsulta']);
+Route::get('/medicos/{id_medico}/pacientes', [CidadeController::class, 'listarPacientesMedicos']);
+
+Route::post('/pacientes', [PacienteController::class, 'store']);
+Route::put('/pacientes/{id_paciente}', [PacienteController::class, 'update']);
